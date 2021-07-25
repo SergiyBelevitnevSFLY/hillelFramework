421eb3f1437b70fc84b77de62ee3a893a68d3950
sergiy.belevitnev <sergiy.belevitnev@shutterfly.com>
2021-07-25 11:31:08 +0300

    lesson 23

--- src/test/java/LoginIUaTestWithPageObject.java
+++ src/test/java/LoginIUaTestWithPageObject.java
@@ -1,17 +1,18 @@
 import base.BaseTest;
+import base.BaseTestWithThreadLocal;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pageObjects.IUALoginPage;
 import pageObjects.IUALoginPageFactory;
 
-public class LoginIUaTestWithPageObject extends BaseTest {
+public class LoginIUaTestWithPageObject extends BaseTestWithThreadLocal {
 
     @Test
     public void successfulLogin(){
         getDriver().get("https://passport.i.ua/login/");
-        String password = System.getProperty("someHillelVariable");
+//        String password = System.getProperty("someHillelVariable");
         IUALoginPage iuaLoginPage = new IUALoginPage(getDriver());
-        iuaLoginPage.login("hillel.summer2021@i.ua", password);
+        iuaLoginPage.login("hillel.summer2021@i.ua", "Summer202!");
     }
 
     @Test
